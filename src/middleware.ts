2671a7cda6427f6b00d5adfbb13725cc3a4d58fc
coryo12345 <corydio12@gmail.com>
2024-01-10 17:01:58 -0500

    checking jwt payload in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,21 @@
 import "server-only";
-// import { verifyJWT } from "@/server/auth";
+
+import { GenericToken, UserToken } from "@/models/auth";
 import { NextResponse, type NextRequest } from "next/server";
-import { User } from "@/models/auth";
 
 const REQUIRE_AUTH_ROUTES = ["/home", "/view", "/answer"];
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("Authorization");
-  if (cookie) {
-    console.log(parseJwtPaylod(cookie.value));
+  if (request.nextUrl.pathname === "/") {
+    if (cookie && !tokenExpired(parseJwtPaylod(cookie.value))) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+  } else if (REQUIRE_AUTH_ROUTES.includes(request.nextUrl.pathname)) {
+    if (!cookie || tokenExpired(parseJwtPaylod(cookie.value))) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
-  // if (request.nextUrl.pathname === "/") {
-  //   const jwtCookie = request.cookies.get("Authorization")
-  //   if (jwtCookie && verifyJWT(jwtCookie.value) !== null) {
-  //     return NextResponse.redirect(new URL("/home", request.url));
-  //   }
-  // } else if (REQUIRE_AUTH_ROUTES.includes(request.nextUrl.pathname)) {
-  //   const jwtCookie = request.cookies.get("Authorization")
-  //   if (!jwtCookie || !verifyJWT(jwtCookie.value)) {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
-  // }
 }
 
 export const config = {
@@ -28,12 +23,11 @@ export const config = {
 };
 
 // https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
-function parseJwtPaylod(token: string): User {
+function parseJwtPaylod(token: string): UserToken {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
+    atob(base64)
       .split("")
       .map((c) => {
         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
@@ -42,4 +36,10 @@ function parseJwtPaylod(token: string): User {
   );
 
   return JSON.parse(jsonPayload);
+}
+
+function tokenExpired(token: GenericToken): boolean {
+  if (!token || !token.exp) return true;
+  const exp = new Date(token.exp * 1000);
+  return exp < new Date();
 }
--- src/models/auth.ts
+++ src/models/auth.ts
@@ -4,3 +4,10 @@ export type User = {
   id: number;
   username: string;
 };
+
+export type GenericToken = {
+  iat: number;
+  exp: number;
+}
+
+export type UserToken = User & GenericToken;
