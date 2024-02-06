a899499fdc4079217baa2113b3cdd18bf4abbb4b
coryo12345 <corydio12@gmail.com>
2024-02-06 14:15:32 -0500

    stub components for new game form

---
+++ src/app/home/games/new/new-game-form.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter } from "@/components/ui/card";
+
+export function NewGameForm() {
+  return (
+    <>
+      <CardContent>
+        <p>todo</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Submit</Button>
+      </CardFooter>
+    </>
+  );
+}
--- src/app/home/games/new/page.tsx
+++ src/app/home/games/new/page.tsx
@@ -1,3 +1,21 @@
+import { NewGameForm } from "@/app/home/games/new/new-game-form";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function NewGamePage() {
-    return <div>new game</div>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>New Game</CardTitle>
+        <CardDescription>
+          Create a new game to play with someone else
+        </CardDescription>
+      </CardHeader>
+      <NewGameForm />
+    </Card>
+  );
 }
